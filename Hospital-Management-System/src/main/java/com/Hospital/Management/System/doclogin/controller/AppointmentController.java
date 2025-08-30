afd5b4a12e8994e47158171f4abfc791b51ada18
Rishabh Kumar <rishabhjuly11@gmail.com>
2025-08-30 22:23:54 +0530

    Implemented DeleteMapping for appointment

--- Hospital-Management-System/src/main/java/com/Hospital/Management/System/doclogin/controller/AppointmentController.java
+++ Hospital-Management-System/src/main/java/com/Hospital/Management/System/doclogin/controller/AppointmentController.java
@@ -1,9 +1,16 @@
 package com.Hospital.Management.System.doclogin.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import javax.management.AttributeNotFoundException;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,14 +33,22 @@ public class AppointmentController {
 
 	@PostMapping("/insert")
 	public Appointment createAppointment(@RequestBody Appointment appointment) {
-		
 		return appointmentsRepository.save(appointment);
 	}
 	
 	@GetMapping
 	public List<Appointment> getAllAppointments() {
-		
 		return appointmentsRepository.findAll();
+	}
+	
+	@DeleteMapping("/appointments/{id}")
+	public ResponseEntity<Map<String, Boolean>> deleteAppointment(@PathVariable long id) throws AttributeNotFoundException {
+		Appointment appointment = appointmentsRepository.findById(id).orElseThrow(() -> new AttributeNotFoundException("Appointment not found with id " + id));
+		appointmentsRepository.delete(appointment);
+		Map<String, Boolean> response = new HashMap<String, Boolean>();
+		response.put("Deleted", Boolean.TRUE);
+		
+		return ResponseEntity.ok(response);
 	}
 	
 }
